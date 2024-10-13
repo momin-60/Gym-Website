b2af8d8ec3eaf7de2a7065e60882467b7b836d1a
momin-60 <mominislam.bd60@gmail.com>
2024-10-13 23:41:38 +0600

    Gym web full responsive

---
+++ Script.js
@@ -0,0 +1,50 @@
+let menubar = document.querySelector('#menubar')
+let navbar = document.querySelector('.navbar')
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let menu = document.querySelector('#mbl');
+// // let nav = document.querySelector('nav')
+
+// // menu.onclick = () =>{
+// //     menu.classList.toggle('fa-times')
+// //     nav.classList.toggle('active')
+// // }
